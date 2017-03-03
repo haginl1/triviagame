c18a2d3c98cf58f9a7a7e2576dec44cc34d40716
Lisa Hagin <lhagin@encompass.tv>
2017-03-03 12:37:43 -0500

    game is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 
-$(function() {//make sure DOM  is ready
-
 //global variables
-var CORRECTANSWERS=0;
-var WRONGANSWERS=0;
-var NOANSWER=0;
-var timer=10;
-var nextQuestionIndex=0;
+var answerTime = 3000;
+var questionTime = 10000;
+var intervalTimer = 1000;
+var timerAnswer="";
+var totalQuestion = 5;
+var interval="";
+var rightAnswer = "";
 var questionsArray=[ {
 			ques: "What is the meaning of life?",
 			choices: ["42", "84", "64", "10"],
@@ -20,66 +20,77 @@ var questionsArray=[ {
 			{
 			ques: "What movie was Bil Paxton NOT in?",
 			choices: ["Twister", "Terminator", "Weird Science", "Alien"],
-			answer:"42"
+			answer:"Alien"
 		},
 			{
 			ques: "What is the average velocity of a (European) laden swallow?",
 			choices: ["36 mph", "24 mph", "11 mph", "100 mph"],
 			answer:"24 mph"
-		},	
+		}	
 ];
 
-		
-		//startup:  Only show a Start button to start the game
-
-			
+function startGame(){
 	
-	
-		//load the question
-		function getQuestion() {
+	getQuestion();
+}
+
+function getQuestion() {
+	$("#correctAnswers").html("");
+	$("#countDown").html(questionTime/intervalTimer);
+	startInterval();
+	clearTimeout(timerAnswer);
+	var randomQuestion = Math.floor((Math.random()* questionsArray.length));
+	rightAnswer = questionsArray[randomQuestion].answer;
+	$("#currentQuestion").html(questionsArray[randomQuestion].ques);
+	// grab the choices
+	for (i=0; i < questionsArray[randomQuestion].choices.length; i++) {
+		$("#RDO"+ i).html('<input type="radio" id="answerRDO' + i + '" onclick="userGuess()" name="answerRDO" value="' + questionsArray[randomQuestion].choices[i] + '" />&nbsp;' + questionsArray[randomQuestion].choices[i]);
+	}
+}
+
+function checkAnswers(answer){
+	$("#correctAnswers").html(rightAnswer);
+	var incrementResult=0;
+	if (answer== "noAnswer"){
+		incrementResult= parseInt($("#noAnswers").text()) +1 ;
+		$("#noAnswers").html(incrementResult);
+	}else if(answer == rightAnswer){
+		incrementResult= parseInt($("#correct").text()) +1 ;
+		$("#correct").html(incrementResult);	
+	}else if (answer != rightAnswer){
+		incrementResult= parseInt($("#wrong").text()) +1 ;
+		$("#wrong").html(incrementResult);
+	}
+	timerAnswer= setTimeout(function(){
+		getQuestion();	
+	}, answerTime);
+}
 
 
-			$("#currentQuestion").append(questionsArray[nextQuestionIndex].ques);
-			
-			$("#correctAnswers").append(questionsArray[nextQuestionIndex].answer);
 
-		}
-		getQuestion();
+function startInterval(){
+    start = parseInt($("#countDown").html());
+    interval = setTimeout(function(){
+      $("#countDown").html(start-1);
+      if($("#countDown").html()!= 0){
+		startInterval();
+	  }else{
+		stopInterval(interval);
+		$("#countDown").html("Time is Up!");
+		checkAnswers("noAnswer");
+	  }
+    }, intervalTimer );
+}
 
+function stopInterval(interval){
+	clearTimeout(interval);
+}
 
+function userGuess(){
 
-		//Let's check if the answers are correct and increment the score and load the next question
-		function checkAnswers(){
-
-			var guess = 0 //need to target the correct answer
-			if (guess === questionsArray[nextQuestionIndex].answer){
-				CORRECTANSWERS++;
-				nextQuestionIndex++;
-				$("#correct").html(CORRECTANSWERS);
-			}
-			else{
-				WRONGANSWERS++;
-				nextQuestionIndex++;
-				$("#wrong").html(WRONGANSWERS);
-			}
-
-			$("#correct").html(CORRECTANSWERS);
-			$("#wrong").html(WRONGANSWERS);
-			$("#noAnswers").html(NOANSWER);
-		}
-		checkAnswers();
-
-
-		//time the user has to select a question
-		function intervalTime(){
-
-		}
-
-		//we'll call the interval timer function to reset the clock betweek each question
-		function startTime(){
-
-
-		} 
-
-});//end document ready function
-
+	stopInterval(interval);
+	$('#countDown').html("");
+	$("input:radio[name='answerRDO']").prop('disabled', true);
+	var answer= $("input:radio[name ='answerRDO']:checked").val();
+	checkAnswers(answer);
+}
