87583600c260bcaf965d5a7f179d2a11f1026857
lhagin@comcast.net <lhagin@comcast.net>
2017-03-03 08:02:11 -0500

    got the questions showing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,26 @@ var CORRECTANSWERS=0;
 var WRONGANSWERS=0;
 var NOANSWER=0;
 var timer=10;
-var nextQuestionIndex=0;
+CURRENTQUESTION=0
 var questionsArray=[ {
 			ques: "What is the meaning of life?",
 			choices: ["42", "84", "64", "10"],
-			answer:"42"
+			answer: 0
 		},
 			{
 			ques: "What sport did Rod Stewart play professionally",
 			choices: ["baseball", "rugby", "soccer", "tennis"],
-			answer:"soccer"
+			answer: "soccer"
 		},
 			{
 			ques: "What movie was Bil Paxton NOT in?",
 			choices: ["Twister", "Terminator", "Weird Science", "Alien"],
-			answer:"42"
+			answer: "Alien"
 		},
 			{
 			ques: "What is the average velocity of a (European) laden swallow?",
 			choices: ["36 mph", "24 mph", "11 mph", "100 mph"],
-			answer:"24 mph"
+			answer: "24"
 		},	
 ];
 
@@ -36,36 +36,39 @@ var questionsArray=[ {
 	
 	
 		//load the question
-		function getQuestion() {
-
-
-			$("#currentQuestion").append(questionsArray[nextQuestionIndex].ques);
+		function getQuestion() {	
 			
-			$("#correctAnswers").append(questionsArray[nextQuestionIndex].answer);
-
+			$("#currentQuestion").append(questionsArray[CURRENTQUESTION].ques);
+			CURRENTQUESTION++;
+			if (CURRENTQUESTION==questionsArray.length) {
+			//push results .append
+			}
+		else {
+			//show next question
+			}
 		}
+		//end time function	{
+			CURRENTQUESTION++;
+			//getquesiton function
+// }
 		getQuestion();
-
-
 
 		//Let's check if the answers are correct and increment the score and load the next question
 		function checkAnswers(){
 
 			var guess = 0 //need to target the correct answer
-			if (guess === questionsArray[nextQuestionIndex].answer){
+			if (guess === questionsArray[i].answer) {
 				CORRECTANSWERS++;
-				nextQuestionIndex++;
-				$("#correct").html(CORRECTANSWERS);
+				$("#correct").append(CORRECTANSWERS);
 			}
 			else{
 				WRONGANSWERS++;
-				nextQuestionIndex++;
-				$("#wrong").html(WRONGANSWERS);
+				$("#wrong").append(WRONGANSWERS);
 			}
 
-			$("#correct").html(CORRECTANSWERS);
-			$("#wrong").html(WRONGANSWERS);
-			$("#noAnswers").html(NOANSWER);
+			$("#correct").append(CORRECTANSWERS);
+			$("#wrong").append(WRONGANSWERS);
+			$("#noAnswers").append(NOANSWER);
 		}
 		checkAnswers();
 
